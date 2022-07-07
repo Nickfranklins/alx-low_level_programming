97da089ab1a782e89f2ad560ea666592a805202b
Nickfranklins <nicfranklinchima@gmail.com>
2022-07-06 20:55:24 -0700

    script to check if a character is an alphabet

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _isalpha - Check if Character is an alphabetic charcter
+ * @m: type into character
+ * Return: Return 1 if character is letter, lowercase or upper case, Retun 0 if otherwise
+ */
+int _isalpha(int_m)
+{
+	if ((m > 64 && m < 91) || (m > 96 && m < 123))
+		return (1);
+	else
+		return (0);
+}
