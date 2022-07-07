687dae09f538923a6155f816a268f851cab4601e
Nickfranklins <nicfranklinchima@gmail.com>
2022-07-06 21:18:15 -0700

    program to check the absolute value of an integer

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _abs - computer the absolute value of an integer
+ * @n: integer type number
+ * Return: absolute value of @n
+ */
+int _abs (int n)
+{
+	if (n < 0)
+	{
+		return (n * -1);
+	}
+	else
+	{
+		retun (n);
+	}
+}
